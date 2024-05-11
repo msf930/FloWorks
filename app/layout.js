cb6ee81721443867022a80b02631dc7a4e2c9d76
Matt Folsom <mattfolsom@Matts-MacBook-Pro.local>
2024-05-11 17:15:57 -0600

    add google verify header

--- app/layout.js
+++ app/layout.js
@@ -2,43 +2,50 @@ import { Roboto_Flex } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/NavBar/NavBar";
 
-
-const roboto_flex = Roboto_Flex({ subsets: ["latin"],display: 'swap' });
+const roboto_flex = Roboto_Flex({ subsets: ["latin"], display: "swap" });
 
 export const metadata = {
   title: "FloWorks Plumbing",
-  description: "Denver local plumbing solutions for your home or business. Expert repairs, installations, and prompt service. Contact us today!",
+  description:
+    "Denver local plumbing solutions for your home or business. Expert repairs, installations, and prompt service. Contact us today!",
   openGraph: {
-    title: 'FloWorks Plumbing',
-    description: 'Denver local plumbing solutions for your home or business. Expert repairs, installations, and prompt service. Contact us today!',
-    url: 'https://floworksplumbing.com',
-    siteName: 'FloWorks Plumbing',
+    title: "FloWorks Plumbing",
+    description:
+      "Denver local plumbing solutions for your home or business. Expert repairs, installations, and prompt service. Contact us today!",
+    url: "https://floworksplumbing.com",
+    siteName: "FloWorks Plumbing",
     images: [
       {
-        url: 'https://floworksplumbing.com/og.png',
+        url: "https://floworksplumbing.com/og.png",
         width: 800,
         height: 600,
-        alt: 'FloWorks Plumbing Logo',
+        alt: "FloWorks Plumbing Logo",
       },
       {
-        url: 'https://floworksplumbing.com/og-alt.png',
+        url: "https://floworksplumbing.com/og-alt.png",
         width: 1800,
         height: 1600,
-        alt: 'FloWorks Plumbing Logo',
+        alt: "FloWorks Plumbing Logo",
       },
     ],
-    locale: 'en_US',
-    type: 'website',
-  }
+    locale: "en_US",
+    type: "website",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
-      <html lang="en">
+    <html lang="en">
+      <head>
+        <meta
+          name="google-site-verification"
+          content="rSQ9LcpuhfFz6g3GksYkiqqIB0BPGz0Dy-7Axvkv8JM"
+        />
+      </head>
       <body className={roboto_flex.className}>
-      <NavBar/>
-      {children}
+        <NavBar />
+        {children}
       </body>
-      </html>
+    </html>
   );
 }
